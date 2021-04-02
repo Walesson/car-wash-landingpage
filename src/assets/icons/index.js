49f1ea1eca999d08144bfb2d7728ddf8a57479e1
Walesson <walesson.silva.job@gmail.com>
2021-04-02 17:21:49 -0300

    chore: final footer session

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,3 +3,6 @@ export { ReactComponent as Drive } from "./driver-pana.svg";
 export { ReactComponent as PlusCircle } from "./plus-circle.svg";
 export { ReactComponent as Persons } from "./persons.svg";
 export { ReactComponent as Start } from "./star.svg";
+export { ReactComponent as Instagram } from "./instagram.svg";
+export { ReactComponent as Linkedin } from "./linkedin.svg";
+export { ReactComponent as Facebook } from "./facebook.svg";
