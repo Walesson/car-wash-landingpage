9dac4835b150eb9e3c234d2ea7586ce4332ab07e
Your Name <you@example.com>
2021-04-11 00:38:23 -0300

    add AboutUS

--- src/components/organisms/AboutUs/AboutUs.js
+++ src/components/organisms/AboutUs/AboutUs.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { Container, TitlePage, Content } from "./styles";
+import { Dashboard, Drive } from "../../../assets/icons";
+import {
+  Container,
+  TitlePage,
+  DescContent,
+  Desc,
+  DescInfo,
+  Title,
+  Info,
+  Banner,
+} from "./styles";
 
-export const AboutUs = () => {
-  return (
-    <Container>
-      <TitlePage>QUEM SOMOS</TitlePage>
-      <Content>Descrição</Content>
-    </Container>
-  );
-};
+export const AboutUs = () => (
+  <Container>
+    <TitlePage>QUEM SOMOS</TitlePage>
+    <DescContent>
+      <Info>
+        <Banner></Banner>
+        <Desc>
+          <DescInfo>
+          <strong>Somos</strong>uma empresa referência em oferecer agilidade e comodidade
+          ao listar parceiros que ofertam serviços de lavagem e estética de
+          automotores, sempre em busca do pioneirismo e da vanguarda, se
+          tratando de lavagem, aliando praticidade com custo e benefício.
+          </DescInfo>
+        </Desc>
+      </Info>
+    </DescContent>
+  </Container>
+);
--- src/components/organisms/AboutUs/styles.js
+++ src/components/organisms/AboutUs/styles.js
@@ -1,19 +1,39 @@
 import styled from "styled-components";
-import { TitlePage as UnstyledTitlePage } from "../../atoms";
+import { H2, Text, TitlePage as UnstyledTitlePage } from "../../atoms";
 import { Container as UnstyledContainer } from "../Container";
 
 export const Container = styled(UnstyledContainer)`
   min-height: 45vh;
-  margin-bottom: 25em;
 `;
 
 export const TitlePage = styled(UnstyledTitlePage)`
   margin-bottom: 2.7em;
 `;
 
-export const Content = styled.div`
-  display: flex;
+export const Title = styled(H2)`
+  color: ${({ theme }) => theme.colors.secondary.main};
+  text-transform: uppercase;
+`;
+
+export const DescContent = styled.div`
   width: 100%;
+  margin-bottom: 18.5em;
+`;
+
+export const Info = styled.div`
+  display: flex;
   justify-content: space-between;
-  margin-bottom: 12em;
+  align-items: center;
+`;
+
+export const Banner = styled.div`
+  width: 42.3em;
+`;
+
+export const Desc = styled.div`
+  max-width: 50em;
+`;
+
+export const DescInfo = styled(Text)`
+  color: ${({ theme }) => theme.colors.text.subtle};
 `;
