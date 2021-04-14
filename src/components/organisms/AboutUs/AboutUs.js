8d648697b364106af4984da999527cc8f33ca6d6
Walesson <walesson.silva.job@gmail.com>
2021-04-13 22:01:30 -0300

    remove component not used

--- src/components/organisms/AboutUs/AboutUs.js
+++ src/components/organisms/AboutUs/AboutUs.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Dashboard, Drive } from "../../../assets/icons";
 import {
   Container,
   TitlePage,
   DescContent,
   Desc,
   DescInfo,
-  Title,
   Info,
   Banner,
 } from "./styles";
@@ -19,10 +17,11 @@ export const AboutUs = () => (
         <Banner></Banner>
         <Desc>
           <DescInfo>
-          <strong>Somos</strong> uma empresa referência em oferecer agilidade e comodidade
-          ao listar parceiros que ofertam serviços de lavagem e estética de
-          automotores, sempre em busca do pioneirismo e da vanguarda, se
-          tratando de lavagem, aliando praticidade com custo e benefício.
+            <strong>Somos</strong> uma empresa referência em oferecer agilidade
+            e comodidade ao listar parceiros que ofertam serviços de lavagem e
+            estética de automotores, sempre em busca do pioneirismo e da
+            vanguarda, se tratando de lavagem, aliando praticidade com custo e
+            benefício.
           </DescInfo>
         </Desc>
       </Info>
